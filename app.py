25b54f9c9360f31af63151f86fc7ab9c7582b48d
raaraya1 <raaraya1@miuandes.cl>
2022-01-28 20:57:19 -0300

    test

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 #cmd =  "sh SCIPOptSuite-7.0.2-Linux-debian.sh"
 os.system("tar xvzf scipoptsuite-7.0.2.tgz")
 os.system("cd scipoptsuite-7.0.2")
+os.system("./configure")
 os.system("make")
 os.system("make gcg")
 
