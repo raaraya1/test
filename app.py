07595d0a4d50f6cc0e87d583e6ee2dae6ecb1fb8
raaraya1 <raaraya1@miuandes.cl>
2022-01-28 21:33:50 -0300

    test

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ import os
 #os.system("cmake --build build")
 os.system("tar xvzf scipoptsuite-7.0.2.tgz")
 os.system("cd scipoptsuite-7.0.2")
+os.system("cd scip")
 os.system("mkdir build")
 os.system("cd build")
 os.system("cmake .. [-DSOPLEX_DIR=/path/to/soplex]")
-
 os.system("make")
+# optional: run a quick check on some instances
 os.system("make check")
-os.system("make install") 
+# optional: install scip executable, library, and headers
+os.system("make install")
+
+
 
 
 #python setup.py install
