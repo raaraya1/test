9c1608e575516f01770b9dbc5ca1b19989d5c46a
raaraya1 <raaraya1@miuandes.cl>
2022-01-28 21:40:22 -0300

    test

--- app.py
+++ app.py
@@ -2,20 +2,9 @@ import streamlit as st
 import subprocess
 import os
 
-#cmd =  "sh SCIPOptSuite-7.0.2-Linux-debian.sh"
-#os.system("cmake -Bbuild -H. [-DSOPLEX_DIR=/path/to/soplex]")
-#os.system("cmake --build build")
-os.system("tar xvzf scipoptsuite-7.0.2.tgz")
-os.system("cd scipoptsuite-7.0.2")
-os.system("cd scip")
-os.system("mkdir build")
-os.system("cd build")
-os.system("cmake .. [-DSOPLEX_DIR=/path/to/soplex]")
-os.system("make")
-# optional: run a quick check on some instances
-os.system("make check")
-# optional: install scip executable, library, and headers
-os.system("make install")
+cmd =  "sh SCIPOptSuite-7.0.2-Linux-debian.sh"
+os.system('cmd')
+
 
 
 
