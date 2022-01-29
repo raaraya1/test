24e71f5f6a44e239f60fa4a684ed39d35e981350
raaraya1 <raaraya1@miuandes.cl>
2022-01-29 12:18:25 -0300

    test

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import subprocess
 import os
 
-os.system("apt-get update")
-os.system("apt-get install build-essential libreadline-dev libz-dev libgmp3-dev lib32ncurses5-dev libboost-program-options-dev")
+#os.system("apt-get update")
+#os.system("apt-get install build-essential libreadline-dev libz-dev libgmp3-dev lib32ncurses5-dev libboost-program-options-dev")
 os.system("cd scipoptsuite-7.0.2")
-os.system("sudo apt-get install cmake")
-os.system("mkdir build")
+#os.system("apt-get install cmake")
+#os.system("mkdir build")
 os.system("cd build")
 os.system("cmake .. | tail -n +91")
 os.system("make")
