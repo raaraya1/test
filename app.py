49a872c22818f782c066e47e73f5fe1c1cc98a1a
raaraya1 <raaraya1@miuandes.cl>
2022-01-29 12:34:45 -0300

    test

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import os
 
 #os.system("apt-get update")
 #os.system("apt-get install build-essential libreadline-dev libz-dev libgmp3-dev lib32ncurses5-dev libboost-program-options-dev")
-os.system("cd scipoptsuite-7.0.2")
+os.system("cd /app/test/scipoptsuite-7.0.2")
 os.system("ls")
-os.system("pwd")
+#os.system("pwd")
 #os.system("apt-get install cmake")
 #os.system("mkdir build")
 #os.system("cd build")
